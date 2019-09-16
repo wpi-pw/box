4186079bc593b5ca1eecda84a011ebe7c6d8b8f6
Dima Minka <bzx@mail.ru>
2019-09-16 11:20:34 +0300

    chore--provision.sh--wordmove removing

--- provision.sh
+++ provision.sh
@@ -48,35 +48,6 @@ echo "=============================="
 sudo usermod -s /bin/bash www-data
 
 echo "=============================="
-echo "Wordmove installation"
-echo "=============================="
-if [ $(gem -v|grep '^2.') ]; then
-	echo "gem installed"
-else
-	sudo apt-get install -y ruby-dev
-	echo "ruby-dev installed"
-	echo "gem not installed"
-	sudo gem install rubygems-update
-	sudo update_rubygems
-fi
-wordmove_install="$(gem list wordmove -i)"
-if [ "$wordmove_install" = true ]; then
-  echo "wordmove installed"
-else
-  echo "wordmove not installed"
-  sudo gem install wordmove
-  wordmove_path="$(gem which wordmove | sed -s 's/.rb/\/deployer\/base.rb/')"
-  if [  "$(grep yaml $wordmove_path)" ]; then
-    echo "can require yaml"
-  else
-    echo "can't require yaml"
-    echo "set require yaml"
-    sudo sed -i "7i require\ \'yaml\'" $wordmove_path
-    echo "can require yaml"
-  fi
-fi
-
-echo "=============================="
 echo "wp cli - ianstall and add bash-completion for user www-data"
 echo "=============================="
 # automatically generate the security keys
